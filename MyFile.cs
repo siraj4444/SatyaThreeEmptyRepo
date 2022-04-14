67c9c837d2e8ac8c4ed92e345b29f321881efed0
siraj <siraj.13207@gmail.com>
2022-04-14 16:01:33 +0530

    first commit

---
+++ MyFile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityDbFirst
+{
+    public partial class TblProducts
+    {
+        
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public int? CategoryId { get; set; }
+
+        public virtual TblCategories Category { get; set; }
+
+        public string Siraj { get;set; }
+        public string MyFile { get;set; }
+    }
+}
---
+++ MyFile2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityDbFirst
+{
+    public partial class TblProducts
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+       
+    }
+}
